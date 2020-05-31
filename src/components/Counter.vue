31ebe122176ccd551adfab247aff651a230c746d
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-05-31 23:35:32 +0200

    vue/composition-api is intalled and simple example added for testing purpose.

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ count }} {{ object.foo }} {{name}}</div>
+</template>
+
+<script>
+import { ref, reactive } from '@vue/composition-api';
+
+export default {
+  props: {
+    name: String,
+  },
+  setup() {
+    const count = ref(0);
+    const object = reactive({ foo: 'bar' });
+
+    // expose to template
+    return {
+      count,
+      object,
+    };
+  },
+};
+</script>
