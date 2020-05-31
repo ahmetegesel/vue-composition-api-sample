7825c17889237e598a70ebd66c72549769ba4d8d
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-06-01 01:50:07 +0200

    News page added and added skeleton for testing reusability of Composition API

---
+++ src/components/entity/ContentDetailPageTemplate.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row no-gutters>
+    <v-col
+      cols="12"
+      sm="2"
+    >
+      <Sidebar>
+        <slot name="left-sidebar"/>
+      </Sidebar>
+    </v-col>
+    <v-col
+      cols="12"
+      sm="8"
+    >
+      <ContentContainer>
+        <slot name="content-container"/>
+      </ContentContainer>
+    </v-col>
+    <v-col
+      cols="12"
+      sm="2"
+    >
+      <Sidebar>
+        <slot name="right-sidebar"/>
+      </Sidebar>
+    </v-col>
+  </v-row>
+</template>
---
+++ src/views/News.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="news">
+    <ContentDetailPageTemplate>
+      <template slot="left-sidebar">
+        Categories
+      </template>
+      <template slot="content-container">
+        Title
+      </template>
+      <template slot="right-sidebar">
+        Content Types
+      </template>
+    </ContentDetailPageTemplate>
+  </div>
+</template>
+
+<script>
+import ContentDetailPageTemplate from '../components/entity/ContentDetailPageTemplate.vue';
+
+export default {
+  name: 'News',
+  components: {
+    ContentDetailPageTemplate,
+  },
+};
+</script>
