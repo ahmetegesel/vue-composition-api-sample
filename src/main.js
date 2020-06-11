d1830479894e43ab1b5b692c2473cf196e3dc01c
Ahmet Egesel <ahmetegesel@hotmail.com>
2020-06-11 21:13:53 +0200

    Extracted Composition API installation to separate folder for future use in tests

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
-import VueCompositionApi from '@vue/composition-api';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill';
-
-Vue.use(VueCompositionApi);
+import './installCompositionApi';
 
 Vue.config.productionTip = false;
 
